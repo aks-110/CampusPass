527b28465b25163c33e0acd22fc159dc396ccf05
aks-110 <aksfromup93@gmail.com>
2026-08-04 17:05:40 +0530

    check

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ const { Server } = require('socket.io');
 // Initialize Socket.io
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // Vite default
+        origin: "http://localhost:5173", 
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
