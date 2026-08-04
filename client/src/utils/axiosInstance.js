b84c93425b095c897026694aea265921893f9fbb
aks-110 <aksfromup93@gmail.com>
2026-08-05 01:13:04 +0530

    loading

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,23 @@
 import axios from 'axios';
 import { store } from '../redux/store';
 import { logout, setCredentials } from '../redux/authSlice';
+import NProgress from 'nprogress';
+
+let activeRequests = 0;
+
+const startLoader = () => {
+  if (activeRequests === 0) {
+    NProgress.start();
+  }
+  activeRequests++;
+};
+
+const stopLoader = () => {
+  activeRequests = Math.max(0, activeRequests - 1);
+  if (activeRequests === 0) {
+    NProgress.done();
+  }
+};
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://localhost:5000/api',
@@ -24,9 +41,24 @@ const processQueue = (error, token = null) => {
   failedQueue = [];
 };
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    startLoader();
+    return config;
+  },
+  (error) => {
+    stopLoader();
+    return Promise.reject(error);
+  }
+);
+
 axiosInstance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    stopLoader();
+    return response;
+  },
   async (error) => {
+    stopLoader();
     const originalRequest = error.config;
     // Avoid retrying on login or refresh routes to prevent loops
     if (error.response?.status === 401 && !originalRequest._retry && !originalRequest.url.includes('/auth/login') && !originalRequest.url.includes('/auth/refresh-token')) {
